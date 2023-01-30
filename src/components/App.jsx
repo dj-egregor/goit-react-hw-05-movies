e3e6525e553a9e13d339d8600c324bbeb93d032f
Alexander Kushnir <dj.egregor1@gmail.com>
2023-01-30 16:25:45 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -34,8 +34,8 @@ export const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="/movies/:movieId/cast" element={<Cast />} />
-          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
         <Route path="*" element={<Home />} />
       </Routes>
